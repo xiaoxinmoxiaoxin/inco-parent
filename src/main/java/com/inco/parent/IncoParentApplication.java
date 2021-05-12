63b18edcb42c3c29dc5a6db89647cf8bb776d37a
bz <1848239485@qq.com>
2021-05-13 00:55:50 +0800

    55

---
+++ src/main/java/com/inco/parent/IncoParentApplication.java
@@ -0,0 +1,13 @@
+package com.inco.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncoParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncoParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inco/parent/IncoParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inco.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncoParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
